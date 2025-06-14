796d1d0cbdc6da77b1fa3976c7c4439989b8b8bc
BEK0013 <mirzabekdaminov@gmail.com>
2025-06-14 20:31:57 +0500

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,11 +1,9 @@
-def get_menu_text():
-    return (
-        "🍗 *Oltin Juja Menyusi:*\n\n"
-        "1. Tovuq – 50,000 so‘m\n"
-        "2. Kartoshka fri – 12,000 so‘m\n"
-        "3. Cola 0.5L – 5,000 so‘m\n"
-        "4. Fanta 0.5L – 5,000 so‘m\n"
-        "5. Sok 0.5L – 10,000 so‘m\n"
-        "6. Non – 5,000 so‘m\n\n"
-        "Buyurtma berish uchun pastdagi tugmani bosing 👇"
-    )
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+def get_main_menu():
+    kb = ReplyKeyboardMarkup(resize_keyboard=True)
+    kb.add(KeyboardButton("🍗 Tovuq menyusi"))
+    kb.add(KeyboardButton("📞 Admin bilan aloqa"))
+    return kb
+
+    
