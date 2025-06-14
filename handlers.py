5de15d699a6043dbeafad1435a8a8ffb164d2159
BEK0013 <mirzabekdaminov@gmail.com>
2025-06-14 20:28:56 +0500

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -7,3 +7,13 @@ async def start_handler(message: types.Message):
 
 def register_handlers(dp: Dispatcher):
     dp.register_message_handler(start_handler, commands=["start"])
+async def start_handler(message: types.Message):
+        await message.answer("\u2705 Buyurtma berish uchun menyudan foydalaning", reply_markup=get_main_menu())
+
+    @dp.message_handler(text="🍗 Tovuq menyusi")
+    async def menu_handler(message: types.Message):
+        await message.answer("1 dona qovurilgan tovuq - 50,000 so‘m\nKartoshka fri - 12,000 so‘m\nCola 0.5L - 5,000 so‘m")
+
+    @dp.message_handler(text="📞 Admin bilan aloqa")
+    async def contact_handler(message: types.Message):
+        await message.answer("Admin bilan bog‘lanish: @BEK_DAMINOV\nTelefon: +998 95 772 99 93")
